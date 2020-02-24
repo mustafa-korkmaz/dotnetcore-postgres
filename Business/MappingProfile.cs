7f813425e1f7cd604a80df2a91eef8da418f00d1
Mustafa Korkmaz <mustafa.korkmaz@scalefocus.com>
2020-02-24 09:59:09 +0200

    automapper problem

--- Business/MappingProfile.cs
+++ Business/MappingProfile.cs
@@ -1,21 +1,23 @@
 ﻿
-using System.Collections.Generic;
+using AutoMapper;
 
 namespace Business
 {
-    public class MappingProfile : AutoMapper.Profile
+    public class MappingProfile : Profile
     {
         public MappingProfile()
         {
             // Add as many of these lines as you need to map your objects
+            CreateMap<Dto.DtoBase, Dal.Models.EntityBase>();
+            CreateMap<Dal.Models.EntityBase, Dto.DtoBase>();
+
 
             CreateMap<Dal.Models.Identity.ApplicationUser, Dto.User.ApplicationUser>();
             CreateMap<Dto.User.ApplicationUser, Dal.Models.Identity.ApplicationUser>();
 
             CreateMap<Dto.Post, Dal.Models.Post>();
             CreateMap<Dal.Models.Post, Dto.Post>();
-            //CreateMap<IEnumerable<Dto.Post>, IEnumerable<Dal.Models.Post>>();
-            //CreateMap<IEnumerable<Dal.Models.Post>, IEnumerable<Dto.Post>>();
+
 
             CreateMap<Dto.Blog, Dal.Models.Blog>();
             CreateMap<Dal.Models.Blog, Dto.Blog>();
