e6d341a03d54409beab1712e474e472568897932
Mustafa Korkmaz <mustafa.korkmaz@scalefocus.com>
2020-02-25 23:48:24 +0200

    Usercontroller require authorization methods added

--- Api/Controllers/UserController.cs
+++ Api/Controllers/UserController.cs
@@ -1,16 +1,19 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using BoilerplateDotnetCorePostgres.Models.User;
 using BoilerplateDotnetCorePostgres.ViewModels.User;
 using Common;
 using Common.Response;
 using Dto.User;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Security;
 
 namespace BoilerplateDotnetCorePostgres.Controllers
 {
-    [Route("")]
+    [Route("[controller]")]
     [ApiController]
+    [Authorize]
     public class UserController : ApiControllerBase
     {
         private readonly ISecurity _security;
@@ -21,6 +24,7 @@ namespace BoilerplateDotnetCorePostgres.Controllers
         }
 
         [HttpPost("token")]
+        [AllowAnonymous]
         public async Task<IActionResult> GetToken([FromBody]TokenModel model)
         {
             var resp = await GetTokenResponse(model);
@@ -33,8 +37,34 @@ namespace BoilerplateDotnetCorePostgres.Controllers
             return Ok(resp);
         }
 
+        [HttpPost("register")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register([FromBody]RegisterModel model)
+        {
+            var resp = await RegisterUser(model);
 
-        private async Task<ApiResponse<TokenViewModel>> GetTokenResponse(TokenModel request)
+            if (resp.Type != ResponseType.Success)
+            {
+                return BadRequest(resp);
+            }
+
+            return Ok(resp);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var resp = await GetUser();
+
+            if (resp.Type != ResponseType.Success)
+            {
+                return BadRequest(resp);
+            }
+
+            return Ok(resp);
+        }
+
+        private async Task<ApiResponse<TokenViewModel>> GetTokenResponse(TokenModel model)
         {
             var apiResp = new ApiResponse<TokenViewModel>
             {
@@ -43,11 +73,11 @@ namespace BoilerplateDotnetCorePostgres.Controllers
 
             var applicationUser = new ApplicationUser
             {
-                Email = request.EmailOrUsername,
-                UserName = request.EmailOrUsername
+                Email = model.EmailOrUsername,
+                UserName = model.EmailOrUsername
             };
 
-            var securityResp = await _security.GetToken(applicationUser, request.Password);
+            var securityResp = await _security.GetToken(applicationUser, model.Password);
 
             if (securityResp.Type != ResponseType.Success)
             {
@@ -55,7 +85,7 @@ namespace BoilerplateDotnetCorePostgres.Controllers
                 return apiResp;
             }
 
-            var model = new TokenViewModel
+            var viewModel = new TokenViewModel
             {
                 Username = applicationUser.UserName,
                 AccessToken = securityResp.Data,
@@ -64,9 +94,65 @@ namespace BoilerplateDotnetCorePostgres.Controllers
                 Id = applicationUser.Id
             };
 
-            apiResp.Data = model;
+            apiResp.Data = viewModel;
             apiResp.Type = ResponseType.Success;
 
+            return apiResp;
+        }
+
+        /// <summary>
+        /// returns user info by identity manager
+        /// </summary>
+        /// <returns></returns>
+        private async Task<ApiResponse<UserViewModel>> GetUser()
+        {
+            var user = await _security.GetUser(User);
+
+            return new ApiResponse<UserViewModel>
+            {
+                Type = ResponseType.Success,
+                Data = new UserViewModel
+                {
+                    Id = user.Id,
+                    CreatedAt = user.CreatedAt,
+                    Email = user.Email,
+                    NameSurname = user.NameSurname,
+                    Username = user.UserName,
+                    EmailConfirmed = user.EmailConfirmed
+                }
+            };
+        }
+
+        /// <summary>
+        /// creates new user
+        /// </summary>
+        /// <param name="model"></param>
+        private async Task<ApiResponse> RegisterUser(RegisterModel model)
+        {
+            var apiResp = new ApiResponse
+            {
+                Type = ResponseType.Fail
+            };
+
+            var applicationUser = new ApplicationUser
+            {
+                Id = Guid.NewGuid().ToString(),
+                Email = model.Email,
+                NameSurname = model.NameSurname,
+                UserName = model.Username,
+                EmailConfirmed = true,
+                CreatedAt = Utility.GetTurkeyCurrentDateTime()
+            };
+
+            var registerResp = await _security.Register(applicationUser, model.Password);
+
+            if (registerResp.Type != ResponseType.Success)
+            {
+                apiResp.ErrorCode = registerResp.ErrorCode;
+                return apiResp;
+            }
+
+            apiResp.Type = ResponseType.Success;
             return apiResp;
         }
 
---
+++ Api/Models/User/RegisterModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BoilerplateDotnetCorePostgres.Models.User
+{
+    public class RegisterModel
+    {
+        [Required]
+        [StringLength(100, ErrorMessage = "{0} field must be max {1} characters.")]
+        public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [StringLength(100, ErrorMessage = "{0} field must be min {2} max {1} characters.", MinimumLength = 4)]
+        public string NameSurname { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
